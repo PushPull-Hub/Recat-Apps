37645cd78a53f0e7cfd907e2e5060d0f8c4e1ef4
Hamza Salvagno <dahhani.hamza1998@gmail.com>
2020-10-22 18:57:20 +0200

    creat a simple react app for front-end

---
+++ React/simple-mern-project/simple-mern-project/frontend/src/App.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react';
+
+import Header from './components/Header/Header';
+import NewProduct from './components/Products/NewProduct';
+import ProductList from './components/Products/ProductList';
+import './App.css';
+
+function App() {
+
+  
+
+  return (
+    <React.Fragment>
+      <Header />
+      <main>
+      <p>Hello World</p>
+      </main>
+    </React.Fragment>
+  );
+}
+
+export default App;
